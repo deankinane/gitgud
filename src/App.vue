21dcfa5a9ee7c4982f721c6627a91b6c22c3937d
Dean Kinane <dean.kinane@gmail.com>
2020-01-26 23:32:59 +0000

    Removed HelloWorld component from main app element

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <TerminalView></TerminalView>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 import TerminalView from "@/components/terminal/TerminalView.vue";
 
 @Component({
   components: {
-    HelloWorld,
     TerminalView
   }
 })
